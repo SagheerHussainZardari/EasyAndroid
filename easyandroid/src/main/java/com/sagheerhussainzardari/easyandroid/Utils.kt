c8803c2e6313f98d965cb6a0ce6bd4b495a4e3e7
mrzardari <sagheerhzardari@gmail.com>
2020-06-29 10:28:20 +0500

    Added Hide and Show For ALl Views2

--- easyandroid/src/main/java/com/sagheerhussainzardari/easyandroid/Utils.kt
+++ easyandroid/src/main/java/com/sagheerhussainzardari/easyandroid/Utils.kt
@@ -1,9 +1,9 @@
 package com.sagheerhussainzardari.easyandroid
 
+
 import android.content.Context
-import android.text.Layout
 import android.view.View
-import android.widget.*
+import android.widget.Toast
 
 fun Context.showToastShort(message: String){
     Toast.makeText(this , message , Toast.LENGTH_SHORT).show()
@@ -13,52 +13,14 @@ fun Context.showToastLong(message: String){
     Toast.makeText(this , message , Toast.LENGTH_LONG).show()
 }
 
-fun LinearLayout.show(){
-    visibility = View.VISIBLE
-}
 
-fun LinearLayout.hide(){
-    visibility = View.VISIBLE
-}
-
-fun ProgressBar.show(){
-    visibility = View.VISIBLE
-}
-
-fun ProgressBar.hide(){
-    visibility = View.VISIBLE
-}
-
-fun EditText.show(){
-    visibility = View.VISIBLE
-}
-
-fun EditText.hide(){
-    visibility = View.VISIBLE
-}
-
-fun Button.show(){
-    visibility = View.VISIBLE
-}
-
-fun Button.hide(){
-    visibility = View.VISIBLE
-}
-
-fun ImageView.show(){
-    visibility = View.VISIBLE
-}
-
-fun ImageView.hide(){
-    visibility = View.VISIBLE
-}
 
 fun View.show(){
     visibility = View.VISIBLE
 }
 
 fun View.hide(){
-    visibility = View.VISIBLE
+    visibility = View.GONE
 }
 
 
