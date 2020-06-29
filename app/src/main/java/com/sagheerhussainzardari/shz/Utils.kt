af6d5569d2839c5e5aadcf61fdc3252cd9327717
mrzardari <sagheerhzardari@gmail.com>
2020-06-29 11:07:28 +0500

    Email and Phone Verifcation Added With Errors Also Shwoing

--- app/src/main/java/com/sagheerhussainzardari/shz/Utils.kt
+++ app/src/main/java/com/sagheerhussainzardari/shz/Utils.kt
@@ -2,9 +2,10 @@ package com.sagheerhussainzardari.shz
 
 
 import android.content.Context
-import android.text.Layout
+import android.util.Patterns
 import android.view.View
-import android.widget.*
+import android.widget.EditText
+import android.widget.Toast
 
 fun Context.showToastShort(message: String){
     Toast.makeText(this , message , Toast.LENGTH_SHORT).show()
@@ -14,12 +15,42 @@ fun Context.showToastLong(message: String){
     Toast.makeText(this , message , Toast.LENGTH_LONG).show()
 }
 
-
-
-fun View.show(){
+fun View.show() {
     visibility = View.VISIBLE
 }
 
-fun View.hide(){
+fun View.hide() {
     visibility = View.GONE
+}
+
+
+fun isEmailValid(email: String): Boolean {
+    return Patterns.EMAIL_ADDRESS.matcher(email).matches()
+}
+
+fun isEmailValid(email: EditText): Boolean {
+
+    if (Patterns.EMAIL_ADDRESS.matcher(email.text.toString()).matches()) {
+        return true
+    } else {
+        email.error = "Enter Valid Email Address!!"
+        email.requestFocus()
+        return false
+    }
+}
+
+
+fun isPhoneValid(phone: String): Boolean {
+    return Patterns.PHONE.matcher(phone).matches()
+}
+
+fun isPhoneValid(phone: EditText): Boolean {
+
+    if (Patterns.EMAIL_ADDRESS.matcher(phone.text.toString()).matches()) {
+        return true
+    } else {
+        phone.error = "Enter Valid Phone Number!!"
+        phone.requestFocus()
+        return false
+    }
 }
--- easyandroid/src/main/java/com/sagheerhussainzardari/easyandroid/Utils.kt
+++ easyandroid/src/main/java/com/sagheerhussainzardari/easyandroid/Utils.kt
@@ -2,27 +2,54 @@ package com.sagheerhussainzardari.easyandroid
 
 
 import android.content.Context
+import android.util.Patterns
 import android.view.View
+import android.widget.EditText
 import android.widget.Toast
 
-fun Context.showToastShort(message: String){
-    Toast.makeText(this , message , Toast.LENGTH_SHORT).show()
+fun Context.showToastShort(message: String) {
+    Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
 }
 
-fun Context.showToastLong(message: String){
-    Toast.makeText(this , message , Toast.LENGTH_LONG).show()
+fun Context.showToastLong(message: String) {
+    Toast.makeText(this, message, Toast.LENGTH_LONG).show()
 }
 
-
-
-fun View.show(){
+fun View.show() {
     visibility = View.VISIBLE
 }
 
-fun View.hide(){
+fun View.hide() {
     visibility = View.GONE
 }
 
+fun isEmailValid(email: String): Boolean {
+    return Patterns.EMAIL_ADDRESS.matcher(email).matches()
+}
+
+fun isEmailValid(email: EditText): Boolean {
+
+    if (Patterns.EMAIL_ADDRESS.matcher(email.text.toString()).matches()) {
+        return true
+    } else {
+        email.error = "Enter Valid Email Address!!"
+        email.requestFocus()
+        return false
+    }
+}
 
 
+fun isPhoneValid(phone: String): Boolean {
+    return Patterns.PHONE.matcher(phone).matches()
+}
 
+fun isPhoneValid(phone: EditText): Boolean {
+
+    if (Patterns.EMAIL_ADDRESS.matcher(phone.text.toString()).matches()) {
+        return true
+    } else {
+        phone.error = "Enter Valid Phone Number!!"
+        phone.requestFocus()
+        return false
+    }
+}
